1cb6b8a26011f73c09bd6db2493006cf6c8e8d01
DanielAlk <daniodfad@yahoo.com.ar>
2016-06-25 00:38:33 -0300

    maquetado

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,26 @@
 class PagesController < ApplicationController
   def home
   end
+  def product
+  end
+  def cart
+  end
+  def catalog
+  end
+  def checkout
+  end
+  def confirm
+  end
+  def partners
+  end
+  def engineering
+  end
+  def instalation
+  end
+  def about
+  end
+  def news
+  end
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
+  get 'producto', to: 'pages#product', as: :product_page
+  get 'carrito', to: 'pages#cart', as: :cart_page
+  get 'catalogo', to: 'pages#catalog', as: :catalog_page
+  get 'checkout', to: 'pages#checkout', as: :checkout_page
+  get 'confirmar', to: 'pages#confirm', as: :confirm_page
+  get 'partners', to: 'pages#partners', as: :partners_page
+  get 'ingenieria', to: 'pages#engineering', as: :engineering_page
+  get 'instalacion', to: 'pages#instalation', as: :instalation_page
+  get 'la-empresa', to: 'pages#about', as: :about_page
+  get 'notiticas', to: 'pages#news', as: :news_page
+  get 'contacto', to: 'pages#contact', as: :contact_page
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
