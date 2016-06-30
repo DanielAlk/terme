e345c00125c1ca4521de12e669964af16183d502
DanielAlk <daniodfad@yahoo.com.ar>
2016-06-30 03:28:10 -0300

    generate devise

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 gem 'figaro'
+gem 'devise', '3.4.1'
 gem 'compass-rails'
 
 group :development, :test do
---
+++ app/assets/javascripts/alerts.js
@@ -0,0 +1,40 @@
+var Alerts = {};
+
+Alerts.notice = function() {
+	var timer;
+	var $alert = $('#alertNotice');
+	var fadeOutAndRemove = function($alert) {
+		if (!$alert.length) return;
+		$alert.fadeOut(function() {
+			if (!$alert.length) return;
+			$alert.remove();
+		});
+	};
+	$alert.find('button.close').click(function(e) {
+		e.preventDefault();
+		fadeOutAndRemove($alert);
+		clearTimeout(timer);
+	});
+	$alert.fadeIn(function() {
+		timer = setTimeout(function() {
+			fadeOutAndRemove($('#alertNotice'));
+		}, 6000);
+	});
+};
+
+Alerts.danger = function(text) {
+	Alerts.generate(text, 'alert-danger');
+};
+
+Alerts.success = function(text) {
+	Alerts.generate(text, 'alert-success');
+};
+
+Alerts.generate = function(text, alertClass) {
+	var alert = '<div class="alert '+ alertClass +' alert-dismissible" role="alert" id="alertNotice" style="display:none;">';
+	alert+= '<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+	alert+=	text;
+	alert+= '</div>';
+	$('body').append(alert);
+	Alerts.notice();
+};
