1d67aae38f4c50c0eb01c47ac4b0395ebf23c476
DanielAlk <daniodfad@yahoo.com.ar>
2016-07-08 06:26:45 -0300

    fix regenerate category slug when title change

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
 	extend FriendlyId
-  friendly_id :title, use: :slugged
+  friendly_id :slug_candidates, use: :slugged
 	has_ancestry
 
 	def path_to_s
@@ -10,5 +10,17 @@ class Category < ActiveRecord::Base
 	def is_not_new_and_has_children?
 		!self.new_record? && self.has_children?
 	end
+
+	private
+		def should_generate_new_friendly_id?
+		  title_changed?
+		end
+
+		def slug_candidates
+		  [
+		    :title,
+		    [ :title, :id ]
+		  ]
+		end
 
 end
