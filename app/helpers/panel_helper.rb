05c946170f5326ad99b4287cf1f4af0247463ea3
DanielAlk <daniodfad@yahoo.com.ar>
2016-07-11 18:44:29 -0300

    unify profile and admin layouts into single panel layout

--- app/helpers/panel_helper.rb
+++ app/helpers/panel_helper.rb
@@ -1,13 +1,5 @@
 module PanelHelper
-	def profile_nav_class(path)
-		if request.path == path
-			'active'
-		elsif controller_name.to_sym == path
-			'active'
-		end
-	end
-
-	def admin_nav_class(path)
+	def panel_nav_class(path)
 		if request.path == path
 			'active'
 		elsif controller_name.to_sym == path
