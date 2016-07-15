5669144901eb8f2730429995fa9dd4265b9211ee
DanielAlk <daniodfad@yahoo.com.ar>
2016-07-15 15:02:07 -0300

    move all pages js to pages.js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,5 +1,39 @@
 var Pages = {};
 
+Pages.home = function() {
+	$('section .owl-carousel').owlCarousel({
+		loop:false,
+		items:1,
+		slideBy:1,
+		margin:0,
+		nav: true,
+		dots: false,
+		responsive:{
+			0:{items:1},
+			650:{items:2},
+			800:{items:3},
+			1025:{items:4},
+			1251:{items:5}
+		}
+	});
+	$('.slider-bottom .owl-carousel').owlCarousel({
+		loop:true,
+		items:1,
+		margin:0,
+		nav:true,
+		dots:true,
+		responsive:{
+			0:{ items:1}
+		}
+	});
+};
+
+Pages.products = function() {
+	$('.products-page-filters select').change(function(e) {
+	  $(this).closest('form').submit();
+	});
+};
+
 Pages.product = function(stock) {
 	AriaProductInit();
 	Pages.quantityPicker(stock);
@@ -26,6 +60,53 @@ Pages.product = function(stock) {
 	    800:{items:3},
 	    1025:{items:4},
 	    1251:{items:5}
+	  }
+	});
+};
+
+Pages.cart = function() {
+	(function() {
+		var el = $('.producto01 .qty');
+		var change = function(amt) { el.val(parseInt(el.val(),10) + amt); };
+  	$('.mas').click(function() { change(1); });
+  	$('.menos').click(function() {change(-1); });
+  })();
+
+  (function() { 
+  	var el = $('.producto02 .qty');
+  	var change = function(amt) { el.val( parseInt( el.val(), 10 ) + amt ); };
+  	$('.mas2').click(function() { change(1); });
+  	$('.menos2').click(function() { change(-1); });
+  })();
+
+  $('section.relacionados .owl-carousel').owlCarousel({
+  	loop:false,
+  	items:1,
+  	slideBy:1,
+  	margin:0,
+  	nav: true,
+  	dots: false,
+  	responsive:{
+  		0:{items:1},
+  		650:{items:2},
+  		800:{items:3},
+  		1025:{items:4},
+  		1251:{items:5}
+  	}
+  });
+};
+
+Pages.carousel = function() {
+	$('.slider.owl-carousel').owlCarousel({
+	  loop:true,
+	  items:1,
+	  margin:0,
+	  nav:true,
+	  dots:true,
+	  autoPlay: 7000,
+	  stopOnHover: true,
+	  responsive:{
+	      0:{ items:1}
 	  }
 	});
 };
